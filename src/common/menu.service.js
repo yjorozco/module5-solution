0e4b10b201799ee31a9ae2d294c91f5c486b5998
yjorozco <yjorozco@cnti.gob.ve>
2017-02-17 17:40:53 -0430

    adding new funct

--- src/common/menu.service.js
+++ src/common/menu.service.js
@@ -8,7 +8,7 @@ angular.module('common')
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
-
+  var user={};
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
       return response.data;
@@ -26,6 +26,24 @@ function MenuService($http, ApiPath) {
       return response.data;
     });
   };
+
+  service.getMenuItem = function (item) {
+    var config = {};
+    if (category) {
+      config.params = {'short_name': item};
+    }
+    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+      return response.data;
+    });
+  };
+
+  service.saveUser = function (userLogin) {
+      service.user=userLogin;
+  };
+
+  service.getUser = function(){
+    return service.user;
+  }
 
 }
 
--- src/public/sing-up/sing-up.controller.js
+++ src/public/sing-up/sing-up.controller.js
@@ -3,11 +3,15 @@
 angular.module('public')
 .controller('SingUpController', SingUpController);
 
-function SingUpController() {
+SingUpController.$inject= ['MenuService'];
+function SingUpController(MenuService) {
   var reg = this;
 
   reg.submit = function () {
     reg.completed = true;
+
+      MenuService.saveUser(reg.user);
+
   };
 }
 
